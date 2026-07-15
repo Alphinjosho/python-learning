3bbfe4bbe2972dbfef70cba5ac172a4f326a3b11
Alphinjosho <153099931+Alphinjosho@users.noreply.github.com>
2026-07-15 23:20:58 +0530

    HTTP modules GET/POST -endponts

--- FAST API/main.py
+++ FAST API/main.py
@@ -9,3 +9,41 @@ def home():
 @app.get("/hello")
 def hello():
     return {"message":"Hello Alphin"}
+
+@app.get("/student")
+def student():
+    return {
+        "name":"Alphin Josho",
+        "age":21,
+        "course":"Ai backend enginer",
+        "collage":"IGNOU-SH"
+    }
+@app.get("/skills")
+def skills():
+    return{
+        "name":"Alphin Josho",
+        "skills" : ["Python","SQL","Docker","Problem Sloving","FastAPI"]
+    }
+
+@app.get("/projects")
+def project():
+    return{
+        "project1": {
+            "project_name": "Student management API",
+            "technology":["FastAPI","Python"],
+            "status":"completed"
+            },
+        "project2": {
+            "project_name": "Smart Notes App",
+            "technology":["FastAPI","React"],
+            "status":"planning"
+            },
+        "project3": {
+            "project_name": "AI Student Assistant",
+            "technology":["FastAPI","LLM","LangChain"],
+            "status":"futuer Project"
+            }
+    }
+@app.post("/student")
+def add_student():
+    return{"message":"I received your request."} 
