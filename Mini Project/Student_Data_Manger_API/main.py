3736ff17e8f84bfeb3628c1e8b9ad8f2fe06b6ac
Alphinjosho <153099931+Alphinjosho@users.noreply.github.com>
2026-07-20 23:51:52 +0530

    now working on mini project

--- Mini Project/Student_Data_Manger_API/main.py
+++ Mini Project/Student_Data_Manger_API/main.py
@@ -1,3 +1,24 @@
 from fastapi import FastAPI
 
-fastapi = FastAPI()
+app = FastAPI()
+
+class Student (BaseModel):
+    name:str
+    age:int
+    course:str
+    collage:str
+
+@app.get("/")
+def home():
+    return{
+        "message":"Student Data Manager API"
+    }
+@app.post("/Student")
+def add_student(new_student:Student):
+    return{
+         new_student.name:name,
+         new_student.age,
+         new_student.course,
+         new_student.collage,
+         
+    }
