a9673f7a5d57f840d16de87442c8b3aa4f6ee325
Alphinjosho <153099931+Alphinjosho@users.noreply.github.com>
2026-07-21 23:46:29 +0530

    Finished week3 / completed Mini project

--- Mini Project/Student_Data_Manger_API/main.py
+++ Mini Project/Student_Data_Manger_API/main.py
@@ -1,24 +1,45 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+students=[]
 
 class Student (BaseModel):
     name:str
     age:int
     course:str
-    collage:str
+    college:str
 
 @app.get("/")
 def home():
     return{
         "message":"Student Data Manager API"
     }
-@app.post("/Student")
+@app.post("/students")
 def add_student(new_student:Student):
-    return{
-         new_student.name:name,
-         new_student.age,
-         new_student.course,
-         new_student.collage,
-         
+    students.append(new_student)
+    return {
+        "name":new_student.name,
+        "age": new_student.age,
+        "course":new_student.course,
+        "college":new_student.college,
+        }
+@app.get("/students")
+def get_students():
+    return students 
+
+@app.get("/students/{student_id}")
+def get_students(student_id:int):
+    return students[student_id]
+    
+@app.put("/students/{student_id}")
+def update_student(student_id:int,updated_student:Student):
+    students[student_id]=updated_student 
+    return updated_student 
+
+@app.delete("/students/{student_id}")
+def delete_student(student_id:int):
+    students.pop(student_id)
+    return {
+        "message":"Student deleted sucessfully"
     }
